e652dbcf1b33fcc191d26f1b1166091cb139aaf4
Keoabetswe <KeoNthite@gmail.com>
2020-09-11 12:10:56 +0200

    update base url

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const api = 
 {
     key:"4ecca3d6a91063bc935517f622228508",
-    baseurl:"https://api.openweathermap.org/data/2.5/"
+    base:"https://api.openweathermap.org/data/2.5/"
 }
 
 const searchBox = document.querySelector('.search-box');
@@ -18,8 +18,8 @@ function setQuery(evt)
 
 function getResults(query)
 {
-    fetch(`${api.base}weather?q={query}&units=metric&APPID=${api.key}`).
-    then(weather => {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
         return weather.json();
     }).then(displayResults);
 }
@@ -35,20 +35,20 @@ function displayResults(weather)
     date.innerText = dateBuilder(now);
 
     let temp = document.querySelector('.current .temp');
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span></span>`;
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
     let hi_low = document.querySelector(('.hi-low'));
-    hi_low.innerText = `{${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+    hi_low.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
 
 function dateBuilder(d)
 {
     let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    let day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
     let day = days[d.getDay()];
     let date = d.getDay();
